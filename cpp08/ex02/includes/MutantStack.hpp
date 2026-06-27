4ee243da3dd4d01c9adc9ab133dbf865eb6bd63d
Oualid Khiar <wkhiar80@gmail.com>
2026-06-27 13:02:51 +0100

    working on ex02

---
+++ cpp08/ex02/includes/MutantStack.hpp
@@ -0,0 +1,16 @@
+#ifndef MUtANTSTACK_HPP
+#define MUtANTSTACK_HPP
+
+#include <iostream>
+#include <stack>
+
+
+class MutantStack : public std::stack {
+    public:
+
+        typedef typename 
+};
+
+
+
+#endif
